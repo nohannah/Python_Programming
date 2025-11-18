7fbb104b5830919299e564b054d9c06b98ca379e
nohannah <nohannah508@gmail.com>
2025-11-18 13:36:24 +0600

    main

--- project/Project.py
+++ project/Project.py
@@ -36,3 +36,43 @@ def withdraw(accounts):
     if account_id not in accounts: 
         print("Account is not found.")
         return 
+    balance = float(intput("Enter withdraw amount:"))
+    if balance > accounts[account_id]["balance"]:
+        print("Insufficient balance.")
+        return
+    else: 
+        accounts[account_id]["balance"] -= balance
+        print("Withdraw successful.")
+        
+def check_balance(accounts):
+    account_id= input("Enter account ID:")
+    if account_id not in accounts: 
+        print("Account is not found.")
+        return 
+    print(f"Account holder :{accounts[account_id]['name']}")
+    print(f"Balance: ${accounts[account_id]['balance']:.2f}")
+    
+def main():
+    accounts = load_accounts()
+    while True: 
+        print("Weocome to Bank System")
+        print("1. Create Account")
+        print("2. Deposit")
+        print("3. Withdraw")
+        print("4. Check Balance")
+        print("5. Exit")
+        choice = input("Enter your choice: ")
+        if choice == '1':
+            create_account(accounts)
+        elif choice == '2':
+            deposit(accounts)
+        elif choice == '3':
+            withdraw(accounts)
+        elif choice == '4':
+            check_balance(accounts)
+        elif choice == '5':
+            save_accounts(accounts)
+            print("Thank you for using the Bank System. Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please try again.")
