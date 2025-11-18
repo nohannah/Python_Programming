c549ce12b3d5165ad8f657c08b2e708c556ab24d
nohannah <nohannah508@gmail.com>
2025-11-18 13:50:41 +0600

    return must be outside the loop

--- project/Project.py
+++ project/Project.py
@@ -1,67 +1,75 @@
 import os
-Data_file="account.txt"
+
+Data_file = "account.txt"
+
 def load_accounts():
-    accounts= {}
+    accounts = {}
     if os.path.exists(Data_file):
         with open(Data_file, 'r') as file:
-            for line in file: 
-                account_id ,name, balance=line.strip().splip(',')
-                accounts[account_id] ={"name" : name , "balance": float(balance)}
-                return accounts
+            for line in file:
+                account_id, name, balance = line.strip().split(',')
+                accounts[account_id] = {"name": name, "balance": float(balance)}
+    return accounts   
+
 def save_accounts(accounts):
-    with open(Data_file, "w") as file : 
-     for account_id, name , balance in accounts.items(): 
-         file.write(f"{account_id},{name},{balance}\n")
+    with open(Data_file, "w") as file:
+        for account_id, data in accounts.items():  
+            file.write(f"{account_id},{data['name']},{data['balance']}\n")
 
 def create_account(accounts):
     account_id = input("Enter account ID: ")
-    if account_id  in accounts: 
+    if account_id in accounts:
         print("Account already exists.")
         return
-    name = input("Enter account holder name:")
-    balance= float(input("Enter the balance: "))
-    
-    accounts[account_id] = { "name" : name , "balance" : balance}
-    print("Account create successfully.")      
+    name = input("Enter account holder name: ")
+    balance = float(input("Enter the opening balance: "))
+
+    accounts[account_id] = {"name": name, "balance": balance}
+    print("Account created successfully.")
+
 def deposit(accounts):
-    account_id= input("Enter account ID:")
-    if account_id not in accounts: 
-        print("Account is not found.")
-        return 
-    balance = float(input("Enter deposit amount:"))  
-    accounts[account_id]["balance"] += balance
-    print("Deposit successfull.")   
+    account_id = input("Enter account ID: ")
+    if account_id not in accounts:
+        print("Account not found.")
+        return
+    amount = float(input("Enter deposit amount: "))
+    accounts[account_id]["balance"] += amount
+    print("Deposit successful.")
+
 def withdraw(accounts):
-    account_id= input("Enter account ID:")
-    if account_id not in accounts: 
-        print("Account is not found.")
-        return 
-    balance = float(intput("Enter withdraw amount:"))
-    if balance > accounts[account_id]["balance"]:
+    account_id = input("Enter account ID: ")
+    if account_id not in accounts:
+        print("Account not found.")
+        return
+    amount = float(input("Enter withdraw amount: "))
+
+    if amount > accounts[account_id]["balance"]:
         print("Insufficient balance.")
         return
-    else: 
-        accounts[account_id]["balance"] -= balance
-        print("Withdraw successful.")
-        
+
+    accounts[account_id]["balance"] -= amount
+    print("Withdraw successful.")
+
 def check_balance(accounts):
-    account_id= input("Enter account ID:")
-    if account_id not in accounts: 
-        print("Account is not found.")
-        return 
-    print(f"Account holder :{accounts[account_id]['name']}")
+    account_id = input("Enter account ID: ")
+    if account_id not in accounts:
+        print("Account not found.")
+        return
+    print(f"Account Holder: {accounts[account_id]['name']}")
     print(f"Balance: ${accounts[account_id]['balance']:.2f}")
-    
+
 def main():
     accounts = load_accounts()
-    while True: 
-        print("Weocome to Bank System")
+    while True:
+        print("\nWelcome to Bank System")
         print("1. Create Account")
         print("2. Deposit")
         print("3. Withdraw")
         print("4. Check Balance")
         print("5. Exit")
+
         choice = input("Enter your choice: ")
+
         if choice == '1':
             create_account(accounts)
         elif choice == '2':
@@ -76,3 +84,5 @@ def main():
             break
         else:
             print("Invalid choice. Please try again.")
+
+main()
